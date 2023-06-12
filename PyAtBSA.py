40c582d9c34f9d3230f3dfea7fefcacbeeb4cadf
Eric <timshull99@icloud.com>
2023-06-12 13:10:09 -0400

    add plot production to .py script, add archive management and cleanup to vcfgen.sh script

--- PyAtBSA.py
+++ PyAtBSA.py
@@ -15,7 +15,13 @@ import numpy as np
 import statsmodels.api as sm
 import plotly.express as px
 import plotly.graph_objects as go
+<<<<<<< HEAD
 from scipy.interpolate import interp1d
+=======
+import plotly.io as pio
+import scipy
+from scipy.signal import find_peaks
+>>>>>>> ee0054a (add plot production to .py script, add archive management and cleanup to vcfgen.sh script)
 import fnmatch
 
 #welcome, print delimeter with delightful ascii DNA "art"
@@ -87,6 +93,7 @@ for key in lines_dict:
 #for key in lines_dict:
 #	count_vcfs
 #	subprocess.call(['./code/VCFgen.sh', key, paired])
+<<<<<<< HEAD
 
 
 # files_vcf = [os.path.basename(x) for x in glob.glob('./VCFs/*')]
@@ -137,3 +144,180 @@ print("""
 # 		fig.add_traces(go.Scatter(x=y_hat[:,0], y=y_hat[:1], name = 'Lowess', line=dict(color='red'), facet_col=i))
 # 		fig.write_image("plotname")
 # #add gstatistic
+=======
+
+
+# files_vcf = [os.path.basename(x) for x in glob.glob('./VCFs/*')]
+
+
+#######data analysis########
+print("""
+    >=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<
+	         -. .-.   .-. .-.   .-. .-.   .  
+	         ||\|||\ /|||\|||\ /|||\|||\ /|
+	         |/ \|||\|||/ \|||\|||/ \|||\||
+	         ~   `-~ `-`   `-~ `-`   `-~ `-
+    >=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<
+	 Producing plots and identifying putative causal mutations
+    >=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<
+	""")
+
+# lowess = sm.nonparametric.lowess
+
+for key in lines_dict:
+	vcftable = "./output/archive/" + key/ + key + ".ems.table"
+	plotname = "./output/archive/" + key/ + key + ".BSA_linkage_map" + ".png"
+	finaltablename = "./output/archive/" + key/ + key + ".complete_table" + ".tsv"
+	df = pd.read_csv(vcftable, sep="\t", header=0)
+
+	#calculate ratio, add to dataframe and calculate mean ratio for peak threshold later. 
+	df['ratio'] = ((df.wt_ref)/(df.wt_ref+df.wt_alt))-((df.mu_ref)/(df.mu_ref+df.mu_alt))
+	ratio_mean = df["ratio"].mean()
+
+	#fit LOWESS curve for each chromosome, add fitted values to dataframe and identify peaks
+	chr_facets=df["chr"].unique()
+	df_list = []
+	lowess = sm.nonparametric.lowess
+
+	for i in chr_facets:
+    
+    	df_chr = df[df['chr']==i].copy()
+    
+    	X=df_chr['pos'].values
+    
+    	Y=df_chr['ratio'].values
+    
+    	y_hat = lowess(Y,X, frac=0.29)[:,1]
+    
+    	df_chr['yhat'] = y_hat
+    
+    	df_list.append(df_chr)
+    
+    	signal = df_chr['yhat'].to_numpy().flatten()
+    
+    	peaks = scipy.signal.find_peaks(signal, height=ratio_mean, 
+                            threshold=None, 
+                            distance=None, 
+                            prominence=None, 
+                            width=None, 
+                            wlen=None, 
+                            rel_height=0.5, 
+                            plateau_size=None)
+    
+    	h = peaks[1]['peak_heights']
+    
+    	if len(h) > 0:
+        	h.sort()
+        	max = h[0]
+        	min = h[-1]
+    	else:
+        	max = 0
+        	min = 0
+    
+    	df_chr['peak'] = [1 if (np.isclose(max, x) or (max < x)) and (np.isclose(min, x) or (min > x))
+                      	else 0 for x in df_chr['yhat']]
+    
+    df = pd.concat(df_list)
+
+	df_peaks = df.loc[df['peak'] == 1]
+
+	df.to_csv('file_name.tsv', sep='\t')
+
+	chr_facets_p=df_peaks["chr"].unique()
+
+	fig = px.scatter(df, x=df['pos'], y=df['ratio'],
+    	facet_col="chr",
+    	opacity=0.8,
+   		color_discrete_sequence=['goldenrod'],
+    	trendline="lowess",
+    	trendline_options=dict(frac=0.29),
+    	trendline_color_override="blue")
+            
+	fig.add_trace(go.Scatter(x=[1],
+    	y=[1],
+    	mode='lines',
+    	name='Lowess Fitted Ratio',
+    	line=dict(color="blue")))
+            
+	fig.update_layout(dict(plot_bgcolor = 'white'))
+	fig.update_xaxes(matches=None)
+	fig.for_each_xaxis(lambda xaxis: xaxis.update(showticklabels=True))
+	fig.for_each_xaxis(lambda x: x.update(title = ''))
+	fig.for_each_yaxis(lambda y: y.update(title = ''))
+
+	fig.add_annotation(
+    	showarrow=False,
+    	xanchor='center',
+    	xref='paper', 
+    	x=0.5, 
+    	yref='paper',
+    	y=-0.12,
+    	text='Position'
+	)
+
+	fig.add_annotation(
+    	showarrow=False,
+    	xanchor='center',
+    	xref='paper', 
+    	x=-0.065, 
+    	yanchor='middle',
+    	yref='paper',
+    	y=0.6,
+    	textangle=-90,
+    	text='Ratio'
+	)
+
+
+	fig.update_xaxes(showgrid=True, 
+	    gridwidth=0.5, 
+	    gridcolor='lightgrey')
+
+	fig.update_xaxes(showline=True, 
+	    linewidth=1, 
+	    linecolor='black')
+
+	fig.update_xaxes(rangemode="tozero")
+
+	fig.update_yaxes(showgrid=True, 
+	    gridwidth=0.5, 
+	    gridcolor='lightgrey')
+
+	fig.update_yaxes(showline=True, 
+	    linewidth=1, 
+	    linecolor='black')
+
+	fig.update_yaxes(range=[0, 0.8])
+
+	fig.update_layout(title=dict(text="Linkage map",
+	    font=dict(color='black')))
+
+	fig.update_traces(marker=dict(size=1))
+
+	fig['data'][0]['showlegend'] = True
+	fig['data'][0]['name'] = 'Polymorphism ratio'
+
+	max_pos=df_peaks['pos'].max()
+	min_pos=df_peaks['pos'].min()
+	max_yhat_pos=df_peaks.loc[df_peaks['yhat'] == df_peaks['yhat'].max()]['pos'].values[0]
+
+	for i in chr_facets_p:
+	    fig.add_vrect(x0=min_pos, x1=max_pos, col=i,
+	        fillcolor="red", opacity=0.2, line_width=0)
+	    fig.add_vline(x=max_yhat_pos, 
+	                  line_dash="dot", 
+	                  col=i, 
+	                  line_width=1)
+	    
+	fig.add_trace(go.Scatter(x=[0,0], 
+	    y=[0,0], 
+	    mode='lines', 
+	    line=dict(color='black', width=1, dash='dot'),
+	    name='Identified Peak',))
+
+	pio.write_image(fig, plotname, scale=6, width=1080, height=1080)
+	df_peaks.to_csv(finaltablename, sep='\t')
+	
+# #add gstatistic
+
+
+>>>>>>> ee0054a (add plot production to .py script, add archive management and cleanup to vcfgen.sh script)
--- code/VCFgen.sh
+++ code/VCFgen.sh
@@ -5,13 +5,29 @@ source ./code/variables.sh
 echo	"    Preparing reference"
 echo	"    >=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<"
 
-# #If reference genome is missing, make a new one 
-# if ! [ -f ./references/$my_species.fa ]; then
-#   curl -o ./references/$my_species.fa.gz $reference_genome_source
-#   gzip -d ./references/$my_species.fa.gz
-# fi
+if [ ! -d "./output/archive" ]; then
+	mkdir ./output/archive
+fi 
 
-# awk '/[Ss]caffold/ || /[Cc]ontig/ {exit} {print}' ./references/$my_species.fa > ./references/$my_species.chrs.fa
+if [ ! -d "./output/archive/${1}" ]; then
+	mkdir ./output/archive/${1}
+fi
+
+if [ ! -d "./output/archive/VCFs" ]; then
+	mkdir ./output/archive/VCFs
+fi
+
+if [ ! -d "./output/archive/${1}/snpEff" ]; then
+	mkdir ./output/archive/${1}/snpEff
+fi
+
+#If reference genome is missing, make a new one 
+if ! [ -f ./references/$my_species.fa ]; then
+  curl -o ./references/$my_species.fa.gz $reference_genome_source
+  gzip -d ./references/$my_species.fa.gz
+fi
+
+awk '/[Ss]caffold/ || /[Cc]ontig/ {exit} {print}' ./references/$my_species.fa > ./references/$my_species.chrs.fa
 fa=./references/$my_species.chrs.fa
 
 # #creating .fai and index files
@@ -19,65 +35,72 @@ fa=./references/$my_species.chrs.fa
 # bwa index -p ./references/$my_species.chrs.fa -a is $fa
 
 #create dictory for gatk haplotype caller
+<<<<<<< HEAD
 picard CreateSequenceDictionary -R .fa -O ./references/$my_species.chrs.dict
+=======
+if [ ! -d "./references/$my_species.chrs.dict" ]; then
+	picard CreateSequenceDictionary -R .fa -O ./references/$my_species.chrs.dict
+fi
+>>>>>>> ee0054a (add plot production to .py script, add archive management and cleanup to vcfgen.sh script)
 
 echo	"    >=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<"
 echo	"    Preparing VCF for $1"
 echo	"    >=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<"
 echo    "    reads seem to be $2"
 
-# #set input files as paired-end or single-read
-# if [ "$2" == "paired-end" ]; then
-#   fa_in_wt="./input/$1_1.wt.fq.gz ./input/$1_2.wt.fq.gz"
-#   fa_in_mu="./input/$1_1.mu.fq.gz ./input/$1_2.mu.fq.gz"
-# fi
+#set input files as paired-end or single-read
+if [ "$2" == "paired-end" ]; then
+  fa_in_wt="./input/$1_1.wt.fq.gz ./input/$1_2.wt.fq.gz"
+  fa_in_mu="./input/$1_1.mu.fq.gz ./input/$1_2.mu.fq.gz"
+fi
 
-# if [ "$2" == "single-read" ]; then
-#   fa_in_wt="./input/$1.wt.fq.gz"
-#   fa_in_wt="./input/$1.mu.fq.gz"
-# fi
+if [ "$2" == "single-read" ]; then
+  fa_in_wt="./input/$1.wt.fq.gz"
+  fa_in_wt="./input/$1.mu.fq.gz"
+fi
 
-# #mapping
-# bwa mem -t $threads -M $fa $fa_in_wt > ./output/$1_wt.sam &
-# bwa mem -t $threads -M $fa $fa_in_mu > ./output/$1_mu.sam
+#mapping
+bwa mem -t $threads -M $fa $fa_in_wt > ./output/$1_wt.sam &
+bwa mem -t $threads -M $fa $fa_in_mu > ./output/$1_mu.sam
 
 echo	"    >=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<"
 echo	"    converting sam to bam"
 echo	"    >=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<"
 
-# samtools view -bSh -@ $threads ./output/$1_mu.sam > ./output/$1_mu.bam &
-# samtools view -bSh -@ $threads ./output/$1_wt.sam > ./output/$1_wt.bam
-# wait
+samtools view -bSh -@ $threads ./output/$1_mu.sam > ./output/$1_mu.bam &
+samtools view -bSh -@ $threads ./output/$1_wt.sam > ./output/$1_wt.bam
+wait
 
-# #fix paired end
-# if [ "$2" == "paired-end" ]; then
-# 	samtools fixmate ./output/$1_mu.bam ./output/$1_mu.fix.bam &
-# 	samtools fixmate ./output/$1_wt.bam ./output/$1_wt.fix.bam
-# fi
+#fix paired end
+if [ "$2" == "paired-end" ]; then
+	samtools fixmate ./output/$1_mu.bam ./output/$1_mu.fix.bam &
+	samtools fixmate ./output/$1_wt.bam ./output/$1_wt.fix.bam
+fi
 
 echo	"    >=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<"
 echo	"    Sorting by coordinate"
 echo	"    >=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<"
 
-# #Coordinate sorting
-# picard SortSam I=./output/$1_mu.fix.bam O=./output/$1_mu.sort.bam SORT_ORDER=coordinate &
-# picard SortSam I=./output/$1_wt.fix.bam O=./output/$1_wt.sort.bam  SORT_ORDER=coordinate
-# wait
+#Coordinate sorting
+picard SortSam I=./output/$1_mu.fix.bam O=./output/$1_mu.sort.bam SORT_ORDER=coordinate &
+picard SortSam I=./output/$1_wt.fix.bam O=./output/$1_wt.sort.bam  SORT_ORDER=coordinate
+wait
 
-##consider using sambamba for this step. I hear it is much faster, and this seems to take longer than it should
-# picard MarkDuplicates I=./output/$1_mu.sort.bam O=./output/$1_mu.sort.md.bam METRICS_FILE=./output/$1_mu.matrics.txt ASSUME_SORTED=true &
-# picard MarkDuplicates I=./output/$1_wt.sort.bam O=./output/$1_wt.sort.md.bam METRICS_FILE=./output/$1_wt.matrics.txt ASSUME_SORTED=true
-# wait
+#consider using sambamba for this step. I hear it is much faster, and this seems to take longer than it should
+picard MarkDuplicates I=./output/$1_mu.sort.bam O=./output/$1_mu.sort.md.bam METRICS_FILE=./output/$1_mu.matrics.txt ASSUME_SORTED=true &
+picard MarkDuplicates I=./output/$1_wt.sort.bam O=./output/$1_wt.sort.md.bam METRICS_FILE=./output/$1_wt.matrics.txt ASSUME_SORTED=true
+wait
 
-# #add header for gatk
-# picard AddOrReplaceReadGroups I=./output/$1_mu.sort.md.bam O=./output/$1_mu.sort.md.rg.bam RGLB=$1_mu RGPL=illumina RGSM=$1_mu RGPU=run1 SORT_ORDER=coordinate &
-# picard AddOrReplaceReadGroups I=./output/$1_wt.sort.md.bam O=./output/$1_wt.sort.md.rg.bam RGLB=$1_wt RGPL=illumina RGSM=$1_wt RGPU=run1 SORT_ORDER=coordinate
-# wait
+#add header for gatk
+picard AddOrReplaceReadGroups I=./output/$1_mu.sort.md.bam O=./output/$1_mu.sort.md.rg.bam RGLB=$1_mu RGPL=illumina RGSM=$1_mu RGPU=run1 SORT_ORDER=coordinate &
+picard AddOrReplaceReadGroups I=./output/$1_wt.sort.md.bam O=./output/$1_wt.sort.md.rg.bam RGLB=$1_wt RGPL=illumina RGSM=$1_wt RGPU=run1 SORT_ORDER=coordinate
+wait
 
-# picard BuildBamIndex INPUT=./output/$1_mu.sort.md.rg.bam &
-# picard BuildBamIndex INPUT=./output/$1_wt.sort.md.rg.bam
-# wait
+picard BuildBamIndex INPUT=./output/$1_mu.sort.md.rg.bam &
+picard BuildBamIndex INPUT=./output/$1_wt.sort.md.rg.bam
+wait
 
+<<<<<<< HEAD
 #GATK HC Variant calling
 #gatk HaplotypeCaller -R $fa -I ./output/$1_mu.sort.md.rg.bam -I ./output/$1_wt.sort.md.rg.bam -O ./output/$1.hc.vcf -output-mode EMIT_ALL_CONFIDENT_SITES --native-pair-hmm-threads 20 #the last argument is necessary for old sequencing results where the quality scores do not match the HC restriction: https://www.biostars.org/p/94637/; I also tried --fix_misencoded_quality_scores -fixMisencodedQuals from the same link but I received an error message. "Bad input: while fixing mis-encoded base qualities we encountered a read that was correctly encoded; we cannot handle such a mixture of reads so unfortunately the BAM must be fixed with some other tool"
 
@@ -89,6 +112,19 @@ echo	"    >=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<"
 
 # #snpEff
 #snpEff $my_species -s ./output/snpEff_summary.html ./output/$1.hc.vcf > ./output/$1.se.vcf
+=======
+GATK HC Variant calling
+gatk HaplotypeCaller -R $fa -I ./output/$1_mu.sort.md.rg.bam -I ./output/$1_wt.sort.md.rg.bam -O ./output/$1.hc.vcf -output-mode EMIT_ALL_CONFIDENT_SITES --native-pair-hmm-threads 20 #the last argument is necessary for old sequencing results where the quality scores do not match the HC restriction: https://www.biostars.org/p/94637/; I also tried --fix_misencoded_quality_scores -fixMisencodedQuals from the same link but I received an error message. "Bad input: while fixing mis-encoded base qualities we encountered a read that was correctly encoded; we cannot handle such a mixture of reads so unfortunately the BAM must be fixed with some other tool"
+
+###########prepering for R#########################
+Exclude indels from a VCF
+gatk -R $fa -T SelectVariants -V ./output/$1.hc.vcf -O ./output/$1.selvars.vcf --selectTypeToInclude SNP
+
+#now make it into a table
+
+#snpEff
+snpEff $my_species -s ./output/archive/${1}/snpEff/${1}_snpEff_summary.html ./output/$1.hc.vcf > ./output/$1.se.vcf
+>>>>>>> ee0054a (add plot production to .py script, add archive management and cleanup to vcfgen.sh script)
 
 #extract snpEFF data and variant information into a table, remove repetative NaN's and retain only those polymorphisms likely to arise from EMS.
 SnpSift extractFields -s ":" -e "NaN" ./output/$1.se.vcf CHROM POS  REF ALT "ANN[*].GENE" "ANN[*].EFFECT" "ANN[*].HGVS_P" "ANN[*].IMPACT" "GEN[*].GT" "GEN[$1_mu].AD" "GEN[$1_wt].AD" > ./output/$1.table
@@ -96,6 +132,7 @@ SnpSift extractFields -s ":" -e "NaN" ./output/$1.se.vcf CHROM POS  REF ALT "ANN
 grep -e $'G\tA' -e $'C\tT' -e $'A\tG' -e $'T\tC' ./output/$1.table > ./output/$1.ems.table.tmp
 sed -i 's/NaN://g' ./output/$1.ems.table.tmp
 
+<<<<<<< HEAD
 #if [ "$mutation" = 'recessive' ]; then 
 	grep -F -e '1/1:0/1' -e '0/1:0/0' ./output/$1.ems.table.tmp > ./output/$1.ems.table
 #else 
@@ -103,6 +140,14 @@ sed -i 's/NaN://g' ./output/$1.ems.table.tmp
 #fi
 
 rm ./output/*.tmp
+=======
+if [ "$mutation" = 'recessive' ]; then 
+	grep -F -e '1/1:0/1' -e '0/1:0/0' ./output/$1.ems.table.tmp > ./output/$1.ems.table
+else 
+grep -e $'0/1:0/0' -e '1/1:0/0' ./output/$1.ems.table.tmp > ./output/$1.ems.table
+fi
+
+>>>>>>> ee0054a (add plot production to .py script, add archive management and cleanup to vcfgen.sh script)
 awk -i inplace -F'\t' -vOFS='\t' '{ gsub(",", "\t", $9) ; gsub(",", "\t", $10) ; gsub(",", "\t", $11) ; print }' ./output/$1.ems.table
 
 #remove complex genotypes
@@ -112,4 +157,20 @@ awk -i inplace -F'\t' 'NF==13' ./output/$1.ems.table
 awk -i inplace '$1 == ($1+0)' ./output/$1.ems.table
 
 #add headers
+<<<<<<< HEAD
 sed -i '1s/^/'chr'\t'pos'\t'ref'\t'alt'\t'gene'\t'snpEffect'\t'snpVariant'\t'snpImpact'\t'mu:wt_GTpred'\t'wt_alt'\t'wt_ref'\t'mu_alt'\t'mu_ref'\n/' ./output/$1.ems.table
+=======
+sed -i '1s/^/'chr'\t'pos'\t'ref'\t'alt'\t'gene'\t'snpEffect'\t'snpVariant'\t'snpImpact'\t'mu:wt_GTpred'\t'wt_alt'\t'wt_ref'\t'mu_alt'\t'mu_ref'\n/' ./output/$1.ems.table
+
+
+#clean up and organize. Change cleanup variable to "False", or comment out to disable.  
+if [ cleanup=="True" ]; then
+	rm ./output/*.tmp
+	rm *.bam
+	rm *.sam
+	rm ${1}.table
+	rm ${}1}.hc.vcf
+fi
+
+mv ./output/* ./output/archive/${1}
+>>>>>>> ee0054a (add plot production to .py script, add archive management and cleanup to vcfgen.sh script)
