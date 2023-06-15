a0d1c27668c159554b37b072fb81940b293f8662
Eric <timshull99@icloud.com>
2023-06-15 15:11:41 -0400

    Script is now in full working order. Accepts fq.gz (single or multiple) and returns putative causal snps. Brav

--- PyAtBSA.py
+++ PyAtBSA.py
@@ -20,18 +20,19 @@ import plotly.io as pio
 import scipy
 from scipy.signal import find_peaks
 import fnmatch
+import io
 
 #welcome, print delimeter with delightful ascii DNA "art"
 print("""
-	>=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<
-	-. .-.   .-. .-.   .-. .-.   .
-	||\|||\ /|||\|||\ /|||\|||\ /|
-	|/ \|||\|||/ \|||\|||/ \|||\||
-	~   `-~ `-`   `-~ `-`   `-~ `-
-	>=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<
-	Checking files for formating
-	>=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<
-	""")
+>=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<
+-. .-.   .-. .-.   .-. .-.   .-. .-.   .-. .-.   .-. .-.   
+||\|||\ /|||\|||\ /|||\|||\ /|||\|||\ /|||\|||\ /|||\|||\ /
+|/ \|||\|||/ \|||\|||/ \|||\|||/ \|||\|||/ \|||\|||/ \|||\|
+~   `-~ `-`   `-~ `-`   `-~ `-~   `-~ `-`   `-~ `-`   `-~ `
+>=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<
+Welcome to PyAtBSA. Checking files for formating
+>=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<
+""")
 
 #Check if files are formatted properly, and if paired end or not
 single_test = 0
@@ -95,170 +96,507 @@ for key in lines_dict:
 
 #######data analysis########
 print("""
-	>=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<
-			 -. .-.   .-. .-.   .-. .-.   .  
-			 ||\|||\ /|||\|||\ /|||\|||\ /|
-			 |/ \|||\|||/ \|||\|||/ \|||\||
-			 ~   `-~ `-`   `-~ `-`   `-~ `-
-	>=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<
-	 Producing plots and identifying putative causal mutations
-	>=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<
-	""")
+>=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<
+-. .-.   .-. .-.   .-. .-.   .-. .-.   .-. .-.   .-. .-.   
+||\|||\ /|||\|||\ /|||\|||\ /|||\|||\ /|||\|||\ /|||\|||\ /
+|/ \|||\|||/ \|||\|||/ \|||\|||/ \|||\|||/ \|||\|||/ \|||\|
+~   `-~ `-`   `-~ `-`   `-~ `-~   `-~ `-`   `-~ `-`   `-~ `
+>=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<
+Producing plots and identifying putative causal mutations
+>=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<
+""")
+######establish functions, exc######
+def gStatistic_Array(o1, o3, o2, o4):
+    # Calculate G-statistc using numpy array input
+    np.seterr(all='ignore')
+
+    e1 = np.where(o1+o2+o3+o4!=0, (o1+o2)*(o1+o3)/(o1+o2+o3+o4), 0)
+    e2 = np.where(o1+o2+o3+o4!=0, (o1+o2)*(o2+o4)/(o1+o2+o3+o4), 0)
+    e3 = np.where(o1+o2+o3+o4!=0, (o3+o4)*(o1+o3)/(o1+o2+o3+o4), 0)
+    e4 = np.where(o1+o2+o3+o4!=0, (o3+o4)*(o2+o4)/(o1+o2+o3+o4), 0)
+
+    llr1 = np.where(o1/e1>0, 2*o1*np.log(o1/e1), 0.0)
+    llr2 = np.where(o2/e2>0, 2*o2*np.log(o2/e2), 0.0)
+    llr3 = np.where(o3/e3>0, 2*o3*np.log(o3/e3), 0.0)
+    llr4 = np.where(o4/e4>0, 2*o4*np.log(o4/e4), 0.0)
+
+    return np.where(e1*e2*e3*e4==0, 0.0, llr1+llr2+llr3+llr4)
 
 lowess = sm.nonparametric.lowess
 
-for key in lines_dict:
-	vcftable = "./output/archive/" + key + "/" + key + ".ems.table"
-	plotname = "./output/archive/" + key + "/" + key + ".BSA_linkage_map" + ".png"
-	finaltablename = "./output/archive/" + key + "/" + key + ".complete_table" + ".tsv"
-	df = pd.read_csv(vcftable, sep="\t", header=0)
+#######Establish variables######
 
-	#calculate ratio, add to dataframe and calculate mean ratio for peak threshold later. 
+for key in lines_dict:
+	vcftable = os.path.join("./output/" + key + "/" + key + ".noknownsnps.table")
+	lowess_plotname = os.path.join("./output/" + key + "/" + key + ".BSA_linkage_map" + ".png")
+	gstat_plotname = os.path.join("./output/" + key + "/" + key + ".BSA_g-stat" + ".png")
+	finalcompletetablename = os.path.join("./output/" + key + "/" + key + ".complete_table" + ".tsv")
+	finallikelycandidatestablename = os.path.join("./output/" + key + "/" + key + ".likely_candidates" + ".tsv")
+
+	#read in ems table
+
+	df = pd.read_csv(vcftable, sep="\t")
+
+	#Calculate delta SNP ratio
 	df['ratio'] = ((df.wt_ref)/(df.wt_ref+df.wt_alt))-((df.mu_ref)/(df.mu_ref+df.mu_alt))
+
+	#Drop indels, leave only SNPs
+	df[
+	    df["ref"].apply(lambda x: len(x) == 1)
+	    & df["alt"].apply(lambda x: len(x) == 1)
+	]
+
+	#Calculate ratio_mean for peak identification lower limit
 	ratio_mean = df["ratio"].mean()
 
-	#fit LOWESS curve for each chromosome, add fitted values to dataframe and identify peaks
+	#df.drop(df[df['ratio'] <= 0.3].index, inplace = True) # drop ratios below the defined value. Sometimes necissary
+
+	#Drop NAs. Infrequently will arise while div/0 seemingly
+	df.dropna(axis=0, how='any', subset="ratio", inplace=True)
+
+	#######lowess smoothing by chromosome######
 	chr_facets=df["chr"].unique()
 	df_list = []
+
 	lowess = sm.nonparametric.lowess
+	lowess_span=0.275
 
 	for i in chr_facets:
-	
-		df_chr = df[df['chr']==i].copy()
-	
-		X=df_chr['pos'].values
-	
-		Y=df_chr['ratio'].values
-	
-		y_hat = lowess(Y,X, frac=0.29)[:,1]
-	
-		df_chr['yhat'] = y_hat
-	
-		df_list.append(df_chr)
-	
-		signal = df_chr['yhat'].to_numpy().flatten()
-	
-		peaks = scipy.signal.find_peaks(signal, height=ratio_mean, 
-							threshold=None, 
-							distance=None, 
-							prominence=None, 
-							width=None, 
-							wlen=None, 
-							rel_height=0.5, 
-							plateau_size=None)
-	
-		h = peaks[1]['peak_heights']
-	
-		if len(h) > 0:
-			h.sort()
-			max = h[0]
-			min = h[-1]
-		else:
-			max = 0
-			min = 0
-	
-		df_chr['peak'] = [1 if (np.isclose(max, x) or (max < x)) and (np.isclose(min, x) or (min > x))
-						else 0 for x in df_chr['yhat']]
-	
+	    
+	    # Fit LOWESS ratio ~ position
+	    df_chr = df[df['chr']==i].copy()
+
+	    X=df_chr['pos'].values
+	    
+	    Y=df_chr['ratio'].values
+	    
+	    y_hat = lowess(Y,X, frac=lowess_span)[:,1]
+	    
+	    df_chr['yhat'] = y_hat # fitted values
+	    
+	    df_list.append(df_chr)
+	    
+	    # Find peaks in the LOWESS fitted curve
+	    signal = df_chr['yhat'].to_numpy().flatten()
+	    
+	    peaks = scipy.signal.find_peaks(
+	        signal, 
+	        height=0.5, 
+	        threshold=None, 
+	        distance=None, 
+	        prominence=None, 
+	        width=None,
+	        wlen=None, 
+	        rel_height=None,
+	        plateau_size=None)
+	    
+	    if len(peaks[0]) > 0:
+	    
+	        pi = peaks[0] 
+	        h = peaks[1]['peak_heights']
+
+	        # Get index of the tallest peak
+	        pi_max = (pi[np.argmax(h)]).flatten()
+
+	        peak_widths = scipy.signal.peak_widths(
+	            signal, 
+	            peaks=pi_max, 
+	            rel_height=0.5, 
+	            prominence_data=None, 
+	            wlen=None)
+
+	        peak_bounds = peak_widths[2:4]
+
+	        peak_bound_min = np.round(peak_bounds[0])
+	        peak_bound_max = np.round(peak_bounds[-1])
+
+	        peak_bound_min_pos = df_chr['pos'].iloc[peak_bound_min].item()
+	        peak_bound_max_pos = df_chr['pos'].iloc[peak_bound_max].item()
+
+	    # label peaks
+	        if len(peak_bounds[0]) != 0:
+	            df_chr['peak'] = [1 if x >= peak_bound_min_pos and x <= peak_bound_max_pos else 0 for x in df_chr['pos']]
+	    
+	    else:
+	        
+	        df_chr['peak'] = 0
+	        
 	df = pd.concat(df_list)
 
 	df_peaks = df.loc[df['peak'] == 1]
 
-	df.to_csv('file_name.tsv', sep='\t')
+	#Calculate G-statistic to glean more info about potential causal SNPs
+	df['G_S'] = gStatistic_Array(df['wt_ref'], df['wt_alt'], df['mu_ref'], df['mu_alt'])
+
+	#calculate 98th percentile of G-statistics
+	GS_98p = np.percentile(df['G_S'], 98)
+
+	df['G_S_98p'] = [1 if (np.isclose(x, GS_98p) or (x > GS_98p))
+	                else 0 for x in df['G_S']]
+
+	dfLC11=df.loc[(df['peak'] == 1) & (df['G_S_98p'] == 1)]
+	dfLC01=df.loc[(df['peak'] == 0) & (df['G_S_98p'] == 1)]
+	dfLC10=df.loc[(df['peak'] == 1) & (df['G_S_98p'] == 0)]
+
+	df_likely_cands = pd.concat([dfLC11, dfLC01, dfLC10])
+
+	#output final table and candidates
+
+	df_likely_cands_sorted = df_likely_cands.sort_values(by = ['G_S', 'ratio', 'peak'], ascending = [False, False, False], na_position = 'first')
+	
+	df_likely_cands_sorted.to_csv(finallikelycandidatestablename, sep='\t', index=False)
+	df.to_csv(finalcompletetablename, sep='\t',index=False)
 
 	chr_facets_p=df_peaks["chr"].unique()
 
-	fig = px.scatter(df, x=df['pos'], y=df['ratio'],
-		facet_col="chr",
-		opacity=0.8,
-		color_discrete_sequence=['goldenrod'],
-		trendline="lowess",
-		trendline_options=dict(frac=0.29),
-		trendline_color_override="blue")
-			
-	fig.add_trace(go.Scatter(x=[1],
-		y=[1],
-		mode='lines',
-		name='Lowess Fitted Ratio',
-		line=dict(color="blue")))
-			
-	fig.update_layout(dict(plot_bgcolor = 'white'))
-	fig.update_xaxes(matches=None)
-	fig.for_each_xaxis(lambda xaxis: xaxis.update(showticklabels=True))
-	fig.for_each_xaxis(lambda x: x.update(title = ''))
-	fig.for_each_yaxis(lambda y: y.update(title = ''))
+	######Generate lowess-ratio graph######
+	chr_facets_p=df_peaks["chr"].unique()
+
+	#establish fig
+	fig = px.scatter(
+	    df,
+	    x=df['pos'],
+	    y=df['ratio'],
+	    facet_col="chr",
+	    opacity=0.8,
+	    color_discrete_sequence=['goldenrod'],
+	    trendline="lowess",
+	    trendline_options=dict(frac=lowess_span),
+	    trendline_color_override="blue",
+	)
+	            
+	#format fig
+	fig.update_traces(
+	    marker=dict(size=3)
+	)
+
+	fig.update_layout(
+	    dict(plot_bgcolor = 'white')
+	)
+
+	fig.update_xaxes(
+	    matches=None
+	)
+
+	fig.for_each_xaxis(
+	    lambda xaxis: xaxis.update(showticklabels=True)
+	)
+
+	fig.for_each_xaxis(
+	    lambda xaxis: fig.update_xaxes(tickangle=0)
+	)
+
+	fig.for_each_xaxis(
+	    lambda x: x.update(title = '')
+	)
+
+	fig.for_each_yaxis(
+	    lambda y: y.update(title = '')
+	)
+
 
 	fig.add_annotation(
-		showarrow=False,
-		xanchor='center',
-		xref='paper', 
-		x=0.5, 
-		yref='paper',
-		y=-0.12,
-		text='Position'
+	    showarrow=False,
+	    xanchor='center',
+	    xref='paper', 
+	    x=0.5, 
+	    yref='paper',
+	    y=-0.1,
+	    text='Position'
 	)
 
 	fig.add_annotation(
-		showarrow=False,
-		xanchor='center',
-		xref='paper', 
-		x=-0.065, 
-		yanchor='middle',
-		yref='paper',
-		y=0.6,
-		textangle=-90,
-		text='Ratio'
+	    showarrow=False,
+	    xanchor='center',
+	    xref='paper', 
+	    x=-0.8, 
+	    yanchor='middle',
+	    yref='paper',
+	    y=0.6,
+	    textangle=-90,
+	    text='Ratio'
 	)
 
 
-	fig.update_xaxes(showgrid=True, 
-		gridwidth=0.5, 
-		gridcolor='lightgrey')
+	fig.update_xaxes(
+	    showgrid=True, 
+	    gridwidth=0.5, 
+	    gridcolor='lightgrey'
+	)
 
-	fig.update_xaxes(showline=True, 
-		linewidth=1, 
-		linecolor='black')
+	fig.update_xaxes(
+	    showline=True, 
+	    linewidth=1, 
+	    linecolor='black'
+	)
 
-	fig.update_xaxes(rangemode="tozero")
+	fig.update_xaxes(
+	    rangemode="tozero"
+	)
 
-	fig.update_yaxes(showgrid=True, 
-		gridwidth=0.5, 
-		gridcolor='lightgrey')
+	fig.update_yaxes(
+	    showgrid=True, 
+	    gridwidth=0.5, 
+	    gridcolor='lightgrey'
+	)
 
-	fig.update_yaxes(showline=True, 
-		linewidth=1, 
-		linecolor='black')
+	fig.update_yaxes(
+	    showline=True, 
+	    linewidth=1, 
+	    linecolor='black'
+	)
 
-	fig.update_yaxes(range=[0, 0.8])
+	fig.update_yaxes(
+	    range=[0, 1]
+	)
 
-	fig.update_layout(title=dict(text="Linkage map",
-		font=dict(color='black')))
+	fig.update_layout(
+	    title=dict(
+	        text="Linkage map",font=dict(color='black')
+	    )
+	)
 
-	fig.update_traces(marker=dict(size=1))
 
 	fig['data'][0]['showlegend'] = True
 	fig['data'][0]['name'] = 'Polymorphism ratio'
 
 	max_pos=df_peaks['pos'].max()
 	min_pos=df_peaks['pos'].min()
-	max_yhat_pos=df_peaks.loc[df_peaks['yhat'] == df_peaks['yhat'].max()]['pos'].values[0]
 
+	        
+	fig.add_trace(
+	go.Scatter(
+	    x=[2],
+	    y=[2],
+	    mode="markers",
+	    name="G-stat > 98 percentile",
+	    line=go.scatter.Line(color="red"),
+	    marker=dict(size=4),
+	    showlegend=True), row=1, col=1
+	)
+
+
+	fig.add_trace(
+	    go.Scatter(
+	        x=[1],
+	        y=[1],
+	        mode='lines',
+	        name='Lowess Fitted Ratio',
+	        line=dict(color="blue")))
+
+	fig.add_trace(
+	    go.Scatter(
+	        x=[0,0], 
+	        y=[0,0], 
+	        mode='lines', 
+	        line=dict(color='black', width=1, dash='dot'),
+	        name='Identified Peak',
+	    )
+	)
+
+	#add g-stat > 98 percentile markers
+	for i in chr_facets:
+	    df_chr = df[df['chr']==i].copy()
+	    outliers = df_chr.loc[df_chr['G_S_98p'] == 1 , ["gene","pos","ratio"]]
+	    fig.add_trace(
+	    go.Scatter(
+	        x=outliers['pos'],
+	        y=outliers['ratio'],
+	        mode="markers",
+	        name="G-stat > 98 percentile",
+	        line=go.scatter.Line(color="red"),
+	        marker=dict(size=4),
+	        showlegend=False), row=1, col=i
+	    )
+	    
+	#add peak markers
 	for i in chr_facets_p:
-		fig.add_vrect(x0=min_pos, x1=max_pos, col=i,
-			fillcolor="red", opacity=0.2, line_width=0)
-		fig.add_vline(x=max_yhat_pos, 
-					  line_dash="dot", 
-					  col=i, 
-					  line_width=1)
-		
+	    df_peaks_chr=df_peaks[df_peaks['chr']==i]
+	    df_chr=df[df['chr'] == i]
+	    max_pos=df_peaks_chr['pos'].max()
+	    min_pos=df_peaks_chr['pos'].min()
+
+	    if max_pos > 0:
+	        max_yhat_pos=df_peaks_chr.loc[df_peaks_chr['yhat'] == df_peaks_chr['yhat'].max()]['pos'].values[0]
+	        fig.add_vrect(
+	            x0=min_pos, 
+	            x1=max_pos, 
+	            col=i,
+	            fillcolor="red", 
+	            opacity=0.2, 
+	            line_width=0
+	        )
+	        
+	        fig.add_vline(
+	            x=max_yhat_pos, 
+	            line_dash="dot", 
+	            col=i, 
+	            line_width=1
+	       )
+
+
+
+
+	pio.write_image(fig, lowess_plotname, scale=6, width=1080, height=540)
+
+	#Generate G-statistic graph
+	#establish g-stat lowess fig
+	fig = px.scatter(df, x=df['pos'], y=df['G_S'],
+	    facet_col="chr",
+	    opacity=0.8,
+	    color_discrete_sequence=['goldenrod'],
+	    trendline="lowess",
+	    trendline_options=dict(frac=lowess_span),
+	    trendline_color_override="blue")
+
+	#format g-stat lowess fig
+	fig.update_traces(
+	    marker=dict(size=3)
+	)
+
+	fig.update_layout(
+	    dict(plot_bgcolor = 'white')
+	)
+
+	fig.update_xaxes(
+	    matches=None
+	)
+
+	fig.for_each_xaxis(
+	    lambda xaxis: xaxis.update(showticklabels=True)
+	)
+
+	fig.for_each_xaxis(
+	    lambda xaxis: fig.update_xaxes(tickangle=0)
+	)
+
+	fig.for_each_xaxis(
+	    lambda x: x.update(title = '')
+	)
+
+	fig.for_each_yaxis(
+	    lambda y: y.update(title = '')
+	)
+
+
+	fig.add_annotation(
+	    showarrow=False,
+	    xanchor='center',
+	    xref='paper', 
+	    x=0.5, 
+	    yref='paper',
+	    y=-0.1,
+	    text='Position'
+	)
+
+	fig.add_annotation(
+	    showarrow=False,
+	    xanchor='center',
+	    xref='paper', 
+	    x=-0.8, 
+	    yanchor='middle',
+	    yref='paper',
+	    y=0.6,
+	    textangle=-90,
+	    text='Ratio'
+	)
+
+
+	fig.update_xaxes(
+	    showgrid=True, 
+	    gridwidth=0.5, 
+	    gridcolor='lightgrey'
+	)
+
+	fig.update_xaxes(
+	    showline=True, 
+	    linewidth=1, 
+	    linecolor='black'
+	)
+
+	fig.update_xaxes(
+	    rangemode="tozero"
+	)
+
+	fig.update_yaxes(
+	    showgrid=True, 
+	    gridwidth=0.5, 
+	    gridcolor='lightgrey'
+	)
+
+	fig.update_yaxes(
+	    showline=True, 
+	    linewidth=1, 
+	    linecolor='black'
+	)
+
+	fig.update_layout(
+	    title=dict(
+	        text="G-statistic Lowess",font=dict(color='black')
+	    )
+	)
+
+
+	fig.add_annotation(
+	    showarrow=False,
+	    xanchor='center',
+	    xref='paper', 
+	    x=0.5, 
+	    yref='paper',
+	    y=-0.1,
+	    text='Position'
+	)
+
+	fig.add_annotation(
+	    showarrow=False,
+	    xanchor='center',
+	    xref='paper', 
+	    x=-0.8, 
+	    yanchor='middle',
+	    yref='paper',
+	    y=0.6,
+	    textangle=-90,
+	    text='G-statistic'
+	)
+
+	fig.add_trace(
+	    go.Scatter(
+	    x=[1],
+	    y=[1],
+	    mode='lines',
+	    name='Lowess Fitted G-stat',
+	    line=dict(color="blue")
+	    )
+	)
+
+
+	fig['data'][0]['showlegend'] = True
+	fig['data'][0]['name'] = 'G-statistic'
+
+	max_pos=df_peaks['pos'].max()
+	min_pos=df_peaks['pos'].min()
+
+	    
 	fig.add_trace(go.Scatter(x=[0,0], 
-		y=[0,0], 
-		mode='lines', 
-		line=dict(color='black', width=1, dash='dot'),
-		name='Identified Peak',))
+	    y=[0,0], 
+	    mode='lines', 
+	    line=dict(color='black', width=1, dash='dot'),
+	    name='Identified Peak',))
 
-	pio.write_image(fig, plotname, scale=6, width=1080, height=1080)
-	df_peaks.to_csv(finaltablename, sep='\t')
-	
- #add gstatistic
 
+	pio.write_image(fig, "./plotname.png", scale=6, width=1080, height=540)
+	df_peaks.to_csv('peaks_file_name.tsv', sep='\t')
+
+
+	pio.write_image(fig, gstat_plotname, scale=6, width=1080, height=540)
+
+print(f'''
+>=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<
+-. .-.   .-. .-.   .-. .-.   .-. .-.   .-. .-.   .-. .-.   
+||\|||\ /|||\|||\ /|||\|||\ /|||\|||\ /|||\|||\ /|||\|||\ /
+|/ \|||\|||/ \|||\|||/ \|||\|||/ \|||\|||/ \|||\|||/ \|||\|
+~   `-~ `-`   `-~ `-`   `-~ `-~   `-~ `-`   `-~ `-`   `-~ `
+>=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<
+Results for {key} generated. 
+>=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<
+''')
--- code/VCFgen.sh
+++ code/VCFgen.sh
@@ -2,24 +2,20 @@
 #pull in variables
 source ./code/variables.sh
 
-echo	"    Preparing references and directory structure"
-echo	"    >=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<"
+echo	"Preparing references and directory structure"
+echo	">=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<"
 
 #make directory structure if it doesn't exist 
-if [ ! -d "./output/archive" ]; then
-	mkdir ./output/archive
-fi 
-
-if [ ! -d "./output/archive/${1}" ]; then
-	mkdir ./output/archive/${1}
+if [ ! -d "./output/$1" ]; then
+	mkdir ./output/$1
 fi
 
-if [ ! -d "./output/archive/VCFs" ]; then
-	mkdir ./output/archive/VCFs
+if [ ! -d "./output/VCFs" ]; then
+	mkdir ./output/VCFs
 fi
 
-if [ ! -d "./output/archive/${1}/snpEff" ]; then
-	mkdir ./output/archive/${1}/snpEff
+if [ ! -d "./output/$1/snpEff" ]; then
+	mkdir ./output/$1/snpEff
 fi
 
 #make reference genome if it doesn't exist 
@@ -46,10 +42,9 @@ if [ ! -f "./references/$my_species.chrs.dict" ]; then
 	picard CreateSequenceDictionary -R .fa -O ./references/$my_species.chrs.dict
 fi
 
-echo	"    >=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<"
-echo	"    Preparing VCF for $1"
-echo	"    >=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<"
-echo    "    reads seem to be $2"
+echo	"References prepared, preparing VCF for $1"
+echo	">=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<"
+echo  "$1 reads seem to be $2"
 
 #set input files as paired-end or single-read
 if [ "$2" == "paired-end" ]; then
@@ -62,92 +57,96 @@ if [ "$2" == "single-read" ]; then
   fa_in_wt="./input/$1.mu.fq.gz"
 fi
 
-#mapping
-#bwa mem -t $threads -M $fa $fa_in_wt > ./output/$1_wt.sam &
-#bwa mem -t $threads -M $fa $fa_in_mu > ./output/$1_mu.sam
+mapping
+bwa mem -t $threads -M $fa $fa_in_wt > ./output/$1/$1_wt.sam &
+bwa mem -t $threads -M $fa $fa_in_mu > ./output/$1/$1_mu.sam
 
-# echo	"    >=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<"
-# echo	"    converting sam to bam"
-# echo	"    >=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<"
+echo	">=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<"
+echo	"Converting sam to bam"
+echo	">=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<"
 
-# samtools view -bSh -@ $threads ./output/$1_mu.sam > ./output/$1_mu.bam &
-# samtools view -bSh -@ $threads ./output/$1_wt.sam > ./output/$1_wt.bam
-# wait
+samtools view -bSh -@ $threads ./output/$1/$1_mu.sam > ./output/$1/$1_mu.bam &
+samtools view -bSh -@ $threads ./output/$1/$1_wt.sam > ./output/$1/$1_wt.bam
+wait
 
-# #fix paired end
-# if [ "$2" == "paired-end" ]; then
-# 	samtools fixmate ./output/$1_mu.bam ./output/$1_mu.fix.bam &
-# 	samtools fixmate ./output/$1_wt.bam ./output/$1_wt.fix.bam
-# fi
-
-# echo	"    >=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<"
-# echo	"    Sorting by coordinate"
-# echo	"    >=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<"
-
-# #Coordinate sorting
-# picard SortSam I=./output/$1_mu.fix.bam O=./output/$1_mu.sort.bam SORT_ORDER=coordinate &
-# picard SortSam I=./output/$1_wt.fix.bam O=./output/$1_wt.sort.bam  SORT_ORDER=coordinate
-# wait
-
-# #consider using sambamba for this step. I hear it is much faster, and this seems to take longer than it should
-# picard MarkDuplicates I=./output/$1_mu.sort.bam O=./output/$1_mu.sort.md.bam METRICS_FILE=./output/$1_mu.matrics.txt ASSUME_SORTED=true &
-# picard MarkDuplicates I=./output/$1_wt.sort.bam O=./output/$1_wt.sort.md.bam METRICS_FILE=./output/$1_wt.matrics.txt ASSUME_SORTED=true
-# wait
-
-# #add header for gatk
-# picard AddOrReplaceReadGroups I=./output/$1_mu.sort.md.bam O=./output/$1_mu.sort.md.rg.bam RGLB=$1_mu RGPL=illumina RGSM=$1_mu RGPU=run1 SORT_ORDER=coordinate &
-# picard AddOrReplaceReadGroups I=./output/$1_wt.sort.md.bam O=./output/$1_wt.sort.md.rg.bam RGLB=$1_wt RGPL=illumina RGSM=$1_wt RGPU=run1 SORT_ORDER=coordinate
-# wait
-
-# picard BuildBamIndex INPUT=./output/$1_mu.sort.md.rg.bam O=./output/${1}_mu.sort.md.rg.bai &
-# picard BuildBamIndex INPUT=./output/$1_wt.sort.md.rg.bam O=./output/${1}_wt.sort.md.rg.bai
-# wait
-
-# #GATK HC Variant calling
-# gatk HaplotypeCaller -R $fa -I ./output/$1_mu.sort.md.rg.bam -I ./output/$1_wt.sort.md.rg.bam -O ./output/$1.hc.vcf -output-mode EMIT_ALL_CONFIDENT_SITES --native-pair-hmm-threads 20
-
-#snpEff, labeling snps with annotations and potential impact on gene function
-snpEff $my_species -s ./output/archive/${1}/snpEff/${1}_snpEff_summary.html ./output/$1.hc.vcf > ./output/$1.se.vcf
-
-echo	"    >=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<"
-echo	"    haplotypes called and snps labeled. Cleaning data...."
-echo	"    >=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<"
-
-#extract snpEFF data and variant information into a table, remove repetative NaN's and retain only those polymorphisms likely to arise from EMS.
-SnpSift extractFields -s ":" -e "NaN" ./output/$1.se.vcf CHROM POS  REF ALT "ANN[*].GENE" "ANN[*].EFFECT" "ANN[*].HGVS_P" "ANN[*].IMPACT" "GEN[*].GT" "GEN[$1_mu].AD" "GEN[$1_wt].AD" > ./output/$1.table
-
-grep -e $'G\tA' -e $'C\tT' -e $'A\tG' -e $'T\tC' ./output/$1.table > ./output/$1.ems.table.tmp
-sed -i 's/NaN://g' ./output/$1.ems.table.tmp
-
-if [ "$mutation" = 'recessive' ]; then 
-	grep -F -e '1/1:0/1' -e '0/1:0/0' ./output/$1.ems.table.tmp > ./output/$1.ems.table
-else 
-	grep -F -e $'0/1:0/0' -e '1/1:0/0' ./output/$1.ems.table.tmp > ./output/$1.ems.table
+#fix paired end
+if [ "$2" == "paired-end" ]; then
+	samtools fixmate ./output/$1/$1_mu.bam ./output/$1/$1_mu.fix.bam &
+	samtools fixmate ./output/$1/$1_wt.bam ./output/$1/$1_wt.fix.bam
 fi
 
-#rm ./output/*.tmp
+echo	"Sorting by coordinate"
+echo	">=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<"
 
-awk -i inplace -F'\t' -vOFS='\t' '{ gsub(",", "\t", $9) ; gsub(",", "\t", $10) ; gsub(",", "\t", $11) ; print }' ./output/$1.ems.table
+#Coordinate sorting
+picard SortSam I=./output/$1/$1_mu.fix.bam O=./output/$1/$1_mu.sort.bam SORT_ORDER=coordinate &
+picard SortSam I=./output/$1/$1_wt.fix.bam O=./output/$1/$1_wt.sort.bam  SORT_ORDER=coordinate
+wait
+
+#consider using sambamba for this step. I hear it is much faster, and this seems to take longer than it should
+picard MarkDuplicates I=./output/$1/$1_mu.sort.bam O=./output/$1/$1_mu.sort.md.bam METRICS_FILE=./output/$1/$1_mu.matrics.txt ASSUME_SORTED=true &
+picard MarkDuplicates I=./output/$1/$1_wt.sort.bam O=./output/$1/$1_wt.sort.md.bam METRICS_FILE=./output/$1/$1_wt.matrics.txt ASSUME_SORTED=true
+wait
+
+#add header for gatk
+picard AddOrReplaceReadGroups I=./output/$1/$1_mu.sort.md.bam O=./output/$1/$1_mu.sort.md.rg.bam RGLB=$1_mu RGPL=illumina RGSM=$1_mu RGPU=run1 SORT_ORDER=coordinate &
+picard AddOrReplaceReadGroups I=./output/$1/$1_wt.sort.md.bam O=./output/$1/$1_wt.sort.md.rg.bam RGLB=$1_wt RGPL=illumina RGSM=$1_wt RGPU=run1 SORT_ORDER=coordinate
+wait
+
+picard BuildBamIndex INPUT=./output/$1/$1_mu.sort.md.rg.bam O=./output/$1/${1}_mu.sort.md.rg.bai &
+picard BuildBamIndex INPUT=./output/$1/$1_wt.sort.md.rg.bam O=./output/$1/${1}_wt.sort.md.rg.bai
+wait
+
+echo	">=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<"
+echo	"Calling haplotypes. This may (will) take awhile..."
+echo	">=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<"
+
+# #GATK HC Variant calling
+# gatk HaplotypeCaller -R $fa -I ./output/$1/$1_mu.sort.md.rg.bam -I ./output/$1/$1_wt.sort.md.rg.bam -O ./output/$1/$1.hc.vcf -output-$1/mode EMIT_ALL_CONFIDENT_SITES --native-pair-hmm-threads 20
+
+#snpEff, labeling snps with annotations and potential impact on gene function
+
+snpEff $my_species -s ./output/$1/snpEff/${1}_snpEff_summary.html ./output/$1/$1.hc.vcf > ./output/$1/$1.se.vcf
+
+echo	">=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<"
+echo	"Haplotypes called and snps labeled. Cleaning data...."
+echo	">=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<"
+
+#extract snpEFF data and variant information into a table, remove repetative NaN's and retain only those polymorphisms likely to arise from EMS.
+SnpSift extractFields -s ":" -e "NaN" ./output/$1/$1.se.vcf CHROM POS  REF ALT "ANN[*].GENE" "ANN[*].EFFECT" "ANN[*].HGVS_P" "ANN[*].IMPACT" "GEN[*].GT" "GEN[$1_mu].AD" "GEN[$1_wt].AD" > ./output/$1/$1.table
+
+grep -e $'G\tA' -e $'C\tT' -e $'A\tG' -e $'T\tC' ./output/$1/$1.table > ./output/$1/$1.ems.table.tmp
+
+sed -i 's/NaN://g' ./output/$1/$1.ems.table.tmp
+
+if [ "$mutation" = 'recessive' ]; then 
+	grep -F -e '1/1:0/1' -e '0/1:0/0' ./output/$1/$1.ems.table.tmp > ./output/$1/$1.ems.table
+else 
+	grep -F -e $'0/1:0/0' -e '1/1:0/0' ./output/$1/$1.ems.table.tmp > ./output/$1/$1.ems.table
+fi
+
+#rm ./output/$1/*.tmp
+
+awk -i inplace -F'\t' -vOFS='\t' '{ gsub(",", "\t", $9) ; gsub(",", "\t", $10) ; gsub(",", "\t", $11) ; print }' ./output/$1/$1.ems.table
 
 #remove complex genotypes
-awk -i inplace -F'\t' 'NF==13' ./output/$1.ems.table
+awk -i inplace -F'\t' 'NF==13' ./output/$1/$1.ems.table
 
 #remove non-numeric chromasomes. This will get rid of chloroplastic and mitochondrial polymorphisms. 
-awk -i inplace '$1 == ($1+0)' ./output/$1.ems.table
+awk -i inplace '$1 == ($1+0)' ./output/$1/$1.ems.table
 
 #remove known snps
-awk 'FNR==NR{a[$1$2];next};!($1$2 in a) || $1~/#CHROM/' $knownsnps ./output/$1.ems.table > ./output/$line.noknownsnps.table
+awk 'FNR==NR{a[$1$2];next};!($1$2 in a) || $1~/#CHROM/' $knownsnps ./output/$1/$1.ems.table > ./output/$1/$1.noknownsnps.table
 
 #add headers
-sed -i '1s/^/'chr'\t'pos'\t'ref'\t'alt'\t'gene'\t'snpEffect'\t'snpVariant'\t'snpImpact'\t'mu:wt_GTpred'\t'wt_alt'\t'wt_ref'\t'mu_alt'\t'mu_ref'\n/' ./output/$line.noknownsnps.table
+sed -i '1s/^/'chr'\t'pos'\t'ref'\t'alt'\t'gene'\t'snpEffect'\t'snpVariant'\t'snpImpact'\t'mu:wt_GTpred'\t'mu_ref'\t'mu_alt'\t'wt_ref'\t'wt_alt'\n/' ./output/$1/$1.noknownsnps.table
 
 #clean up and organize. Change cleanup variable to "False", or comment out to disable.  
 #if [ $cleanup ]; then
-#	rm ./output/*.tmp
-#	rm ./output/*.bam
-#	rm ./output/*.sam
-#	rm ./output/${1}.table
-#	rm ./output/${1}.hc.vcf
+#	rm ./output/$1/*.tmp
+#	rm ./output/$1/*.bam
+#	rm ./output/$1/*.sam
+#	rm ./output/$1/${1}.table
+#	rm ./output/$1/${1}.hc.vcf
 #fi
 
-#mv ./output/* ./output/archive/${1}
