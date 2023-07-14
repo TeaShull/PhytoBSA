334d181ae8bd4aea8ad3f403cc6eeafb3573d8fb
Eric <timshull99@icloud.com>
2023-07-13 23:00:49 -0400

    handled conflict markers

--- PyAtBSA.py
+++ PyAtBSA.py
@@ -87,17 +87,10 @@ lines_dict = list(dict.fromkeys(lines))
 for key in lines_dict:
 	subprocess.call(['./code/VCFgen.sh', key, paired])
 
-<<<<<<< HEAD
 #Create snp mask (not ready yet)
 # for key in lines_dict:
 # 	count_vcfs
 # 	subprocess.call(['./code/VCFgen.sh', key, paired])
-=======
-Create snp mask (not ready yet)
-for key in lines_dict:
-	count_vcfs
-	subprocess.call(['./code/VCFgen.sh', key, paired])
->>>>>>> 18608ea (test)
 
 files_vcf = [os.path.basename(x) for x in glob.glob('./VCFs/*')]
 
