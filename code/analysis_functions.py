c63cd5222b7285334bad4d22f6d3991dedb665df
TeaShull <timshull99@icloud.com>
2023-10-30 16:02:55 -0500

    improved graph generation, tidying

--- code/analysis_functions.py
+++ code/analysis_functions.py
@@ -1,10 +1,12 @@
-# Functions for PyAtBSA analysis subprocess
+import numpy as np
+import statsmodels.api as sm
 
-def deltaSNParray(wtr, wta, mur, mua):
+# Functions for PyAtBSA analysis subprocess
+def delta_snp_array(wtr, wta, mur, mua):
     #Calculate delta-SNP ratio
     return ((wtr)/(wtr+wta))-((mur)/(mur+mua))
 
-def gStatistic_Array(o1, o3, o2, o4):
+def g_statistic_array(o1, o3, o2, o4):
     # Calculate G-statistic using numpy array input
     np.seterr(all='ignore')
 
@@ -20,29 +22,47 @@ def gStatistic_Array(o1, o3, o2, o4):
 
     return np.where(e1*e2*e3*e4==0, 0.0, llr1+llr2+llr3+llr4)
 
-def shuffle(posin, wt, mu):
+def empircal_cutoff(posin, wt, mu):
     # Shuffle phenotype/genotype/position association 1000x
-    dfShPos = posin.sample(frac=1)
-    dfShwt = wt.sample(frac=1)
-    dfShmu = mu.sample(frac=1)
+    # Establish variables
+    lowess = sm.nonparametric.lowess
+    lowess_span=0.3
 
-    smPos = dfShPos['pos'].to_numpy()
-    sm_wt_ref = dfShwt['wt_ref'].to_numpy()
-    sm_wt_alt = dfShwt['wt_alt'].to_numpy()
-    sm_mu_ref = dfShmu['mu_ref'].to_numpy()
-    sm_mu_alt = dfShmu['mu_alt'].to_numpy()
-    
-    #Calculate G-stats per iteration,collect results
-    smGstat = gStatistic_Array(sm_wt_ref, sm_wt_alt, sm_mu_ref, sm_mu_alt)
-    smGstatAll.extend(smGstat)
+    ## Establish lists
+    smGstatAll = []
+    smRatioAll = []
+    RS_GAll = []
+    smRS_G_yhatAll = []
+    smRatio_yhatAll = []
 
-    #Calculate snpRatio per iteration,collect results
-    smRatio = deltaSNParray(sm_wt_ref, sm_wt_alt, sm_mu_ref, sm_mu_alt)
-    smRatioAll.extend(smRatio)
+    for i in range(1000):
+        dfShPos = posin.sample(frac=1)
+        dfShwt = wt.sample(frac=1)
+        dfShmu = mu.sample(frac=1)
 
-    #Calculate ratio-scaled G-stat per iteration,collect results
-    smRS_G = smRatio*smGstat
-    RS_GAll.extend(smRS_G)
+        smPos = dfShPos['pos'].to_numpy()
+        sm_wt_ref = dfShwt['wt_ref'].to_numpy()
+        sm_wt_alt = dfShwt['wt_alt'].to_numpy()
+        sm_mu_ref = dfShmu['mu_ref'].to_numpy()
+        sm_mu_alt = dfShmu['mu_alt'].to_numpy()
+        
+        #Calculate G-stats per iteration,collect results
+        smGstat = g_statistic_array(sm_wt_ref, sm_wt_alt, sm_mu_ref, sm_mu_alt)
+        smGstatAll.extend(smGstat)
 
-    #Lowess smooth per iteration, collect results
-    smRS_G_yhatAll.extend(lowess(smRS_G,smPos, frac=lowess_span)[:,1])
+        #Calculate snpRatio per iteration,collect results
+        smRatio = delta_snp_array(sm_wt_ref, sm_wt_alt, sm_mu_ref, sm_mu_alt)
+        smRatioAll.extend(smRatio)
+
+        #Calculate ratio-scaled G-stat per iteration,collect results
+        smRS_G = smRatio*smGstat
+        RS_GAll.extend(smRS_G)
+
+        #Lowess smooth per iteration, collect results
+        smRS_G_yhatAll.extend(lowess(smRS_G,smPos, frac=lowess_span)[:,1])
+
+    G_S_95p = np.percentile(smGstatAll, 95)
+    RS_G_95p = np.percentile(RS_GAll, 95)
+    RS_G_Y_99p = np.percentile(smRS_G_yhatAll, 99.99)
+
+    return G_S_95p, RS_G_95p, RS_G_Y_99p
