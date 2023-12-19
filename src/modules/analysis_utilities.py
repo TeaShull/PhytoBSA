c4094758271bc0b7a288be539e7a3da571e09483
TeaShull <timshull99@icloud.com>
2023-12-19 13:57:48 -0600

    add more robust error handling, collapse some aspects of analysis script into utility functions in analysis_functions.py module

--- src/modules/analysis_utilities.py
+++ src/modules/analysis_utilities.py
@@ -5,96 +5,128 @@ from plotnine import ggplot, aes, geom_point, geom_line, theme_linedraw, facet_g
 import config
 
 class AnalysisUtilities:
-    def __init__(self):
-        pass  # You can initialize any class-specific variables here
+    LOWESS_SPAN = 0.3
 
-    def abort_analysis(self):
-        print(f"Aborting analysis of {self.current_line_name}")
-        quit()
+    def __init__(self, current_line_name):
+        self.current_line_name = current_line_name
 
     def delta_snp_array(self, wtr, wta, mur, mua):
-        # Calculate delta-SNP ratio
-        return ((wtr)/(wtr+wta))-((mur)/(mur+mua))
+        """Calculate delta-SNP ratio."""
+        try:
+            print(f"Attempting to calculate delta-SNP ratios for {self.current_line_name}...")
+            result = ((wtr) / (wtr + wta)) - ((mur) / (mur + mua))
+            print(f"Delta-SNP ratio calculation successful")
+            return result
+        except Exception as e:
+            print(f"Error in delta_snp_array for {self.current_line_name}: {e}")
+            return None
 
     def g_statistic_array(self, o1, o3, o2, o4):
-        # Calculate G-statistic using numpy array input
-        np.seterr(all='ignore')
+        """Calculate G-statistic using numpy array input."""
+        print(f"Attempting to calculate G-statistics for {self.current_line_name}....")
+        try:
+            np.seterr(all='ignore')
 
-        e1 = np.where(o1+o2+o3+o4!=0, (o1+o2)*(o1+o3)/(o1+o2+o3+o4), 0)
-        e2 = np.where(o1+o2+o3+o4!=0, (o1+o2)*(o2+o4)/(o1+o2+o3+o4), 0)
-        e3 = np.where(o1+o2+o3+o4!=0, (o3+o4)*(o1+o3)/(o1+o2+o3+o4), 0)
-        e4 = np.where(o1+o2+o3+o4!=0, (o3+o4)*(o2+o4)/(o1+o2+o3+o4), 0)
+            nonzero_mask = o1 + o2 + o3 + o4 != 0
 
-        llr1 = np.where(o1/e1>0, 2*o1*np.log(o1/e1), 0.0)
-        llr2 = np.where(o2/e2>0, 2*o2*np.log(o2/e2), 0.0)
-        llr3 = np.where(o3/e3>0, 2*o3*np.log(o3/e3), 0.0)
-        llr4 = np.where(o4/e4>0, 2*o4*np.log(o4/e4), 0.0)
+            e1 = np.where(nonzero_mask, (o1 + o2) * (o1 + o3) / (o1 + o2 + o3 + o4), 0)
+            e2 = np.where(nonzero_mask, (o1 + o2) * (o2 + o4) / (o1 + o2 + o3 + o4), 0)
+            e3 = np.where(nonzero_mask, (o3 + o4) * (o1 + o3) / (o1 + o2 + o3 + o4), 0)
+            e4 = np.where(nonzero_mask, (o3 + o4) * (o2 + o4) / (o1 + o2 + o3 + o4), 0)
 
-        return np.where(e1*e2*e3*e4==0, 0.0, llr1+llr2+llr3+llr4)
+            llr1 = np.where(o1 / e1 > 0, 2 * o1 * np.log(o1 / e1), 0.0)
+            llr2 = np.where(o2 / e2 > 0, 2 * o2 * np.log(o2 / e2), 0.0)
+            llr3 = np.where(o3 / e3 > 0, 2 * o3 * np.log(o3 / e3), 0.0)
+            llr4 = np.where(o4 / e4 > 0, 2 * o4 * np.log(o4 / e4), 0.0)
 
-    def empirical_cutoff(self, posin, wt, mu):
-        # Shuffle phenotype/genotype/position association 1000x
-        # Establish variables
-        lowess = sm.nonparametric.lowess
-        lowess_span = 0.3
+            result = np.where(e1 * e2 * e3 * e4 == 0, 0.0, llr1 + llr2 + llr3 + llr4)
+            print(f"G-statistic calculation successful for {self.current_line_name}")
+            return result
+        except Exception as e:
+            print(f"Error in g_statistic_array for {self.current_line_name}: {e}")
+            return None
 
-        ## Establish lists
-        smGstatAll = []
-        smRatioAll = []
-        RS_GAll = []
-        smRS_G_yhatAll = []
-        smRatio_yhatAll = []
+    def empirical_cutoff(self, posin, wt, mu, shuffle_iterations, lowess_span):
+        """Calculate empirical cutoff."""
+        print(f"Attempting to calculate empirical cutoff for {self.current_line_name}...")
+        try:
+            lowess = sm.nonparametric.lowess
 
-        for i in range(1000):
-            dfShPos = posin.sample(frac=1)
-            dfShwt = wt.sample(frac=1)
-            dfShmu = mu.sample(frac=1)
+            smGstatAll, smRatioAll, RS_GAll, smRS_G_yhatAll = [], [], [], []
 
-            smPos = dfShPos['pos'].to_numpy()
-            sm_wt_ref = dfShwt['wt_ref'].to_numpy()
-            sm_wt_alt = dfShwt['wt_alt'].to_numpy()
-            sm_mu_ref = dfShmu['mu_ref'].to_numpy()
-            sm_mu_alt = dfShmu['mu_alt'].to_numpy()
+            for _ in range(shuffle_iterations):
+                dfShPos = posin.sample(frac=1)
+                dfShwt = wt.sample(frac=1)
+                dfShmu = mu.sample(frac=1)
 
-            # Calculate G-stats per iteration, collect results
-            smGstat = self.g_statistic_array(sm_wt_ref, sm_wt_alt, sm_mu_ref, sm_mu_alt)
-            smGstatAll.extend(smGstat)
+                smPos = dfShPos['pos'].to_numpy()
+                sm_wt_ref = dfShwt['wt_ref'].to_numpy()
+                sm_wt_alt = dfShwt['wt_alt'].to_numpy()
+                sm_mu_ref = dfShmu['mu_ref'].to_numpy()
+                sm_mu_alt = dfShmu['mu_alt'].to_numpy()
 
-            # Calculate snpRatio per iteration, collect results
-            smRatio = self.delta_snp_array(sm_wt_ref, sm_wt_alt, sm_mu_ref, sm_mu_alt)
-            smRatioAll.extend(smRatio)
+                smGstat = self.g_statistic_array(sm_wt_ref, sm_wt_alt, sm_mu_ref, sm_mu_alt)
+                smGstatAll.extend(smGstat)
 
-            # Calculate ratio-scaled G-stat per iteration, collect results
-            smRS_G = smRatio * smGstat
-            RS_GAll.extend(smRS_G)
+                smRatio = self.delta_snp_array(sm_wt_ref, sm_wt_alt, sm_mu_ref, sm_mu_alt)
+                smRatioAll.extend(smRatio)
 
-            # Lowess smooth per iteration, collect results
-            smRS_G_yhatAll.extend(lowess(smRS_G, smPos, frac=lowess_span)[:, 1])
+                smRS_G = smRatio * smGstat
+                RS_GAll.extend(smRS_G)
 
-        G_S_95p = np.percentile(smGstatAll, 95)
-        RS_G_95p = np.percentile(RS_GAll, 95)
-        RS_G_Y_99p = np.percentile(smRS_G_yhatAll, 99.99)
+                smRS_G_yhatAll.extend(lowess(smRS_G, smPos, frac=lowess_span)[:, 1])
 
-        return G_S_95p, RS_G_95p, RS_G_Y_99p
+            G_S_95p = np.percentile(smGstatAll, 95)
+            RS_G_95p = np.percentile(RS_GAll, 95)
+            RS_G_Y_99p = np.percentile(smRS_G_yhatAll, 99.99)
+
+            result = G_S_95p, RS_G_95p, RS_G_Y_99p
+            print(f"Empirical cutoff calculation successful for {self.current_line_name}")
+            return result
+        except Exception as e:
+            print(f"Error in empirical_cutoff for {self.current_line_name}: {e}")
+            return None, None, None
 
     def plot_data(self, df, y_column, title_text, ylab_text, cutoff_value=None, lines=False):
-        df['pos_mb'] = df['pos']*0.000001
-        chart = ggplot(df, aes('pos_mb', y=y_column))
-        title = ggtitle(title_text)
-        axis_x = xlab("Position (Mb)")
-        axis_y = ylab(ylab_text)
+        """Plot data."""
+        print(f"Attempting to plot data for {self.current_line_name}...")
+        try:
+            df['pos_mb'] = df['pos'] * 0.000001
+            chart = ggplot(df, aes('pos_mb', y=y_column))
+            title = ggtitle(title_text)
+            axis_x = xlab("Position (Mb)")
+            axis_y = ylab(ylab_text)
 
-        if cutoff_value is not None:
-            cutoff = geom_hline(yintercept=cutoff_value, color='red', linetype="dashed", size=0.3)
-            plot = chart + geom_point(color='goldenrod', size=0.8) + theme_linedraw() + facet_grid('. ~ chr', space='free_x', scales='free_x') + title + axis_x + axis_y + theme(panel_spacing=0.025) + cutoff
-        else:
-            plot = chart + geom_point(color='goldenrod', size=0.8) + theme_linedraw() + facet_grid('. ~ chr', space='free_x', scales='free_x') + title + axis_x + axis_y + theme(panel_spacing=0.025)
+            if cutoff_value is not None:
+                cutoff = geom_hline(yintercept=cutoff_value, color='red',
+                                    linetype="dashed", size=0.3
+                                    )
+                plot = (chart
+                        + geom_point(color='goldenrod', size=0.8)
+                        + theme_linedraw()
+                        + facet_grid('. ~ chr', space='free_x', scales='free_x')
+                        + title
+                        + axis_x
+                        + axis_y
+                        + theme(panel_spacing=0.025)
+                        + cutoff
+                        )
+            else:
+                plot = (chart
+                        + geom_point(color='goldenrod', size=0.8)
+                        + theme_linedraw()
+                        + facet_grid('. ~ chr', space='free_x', scales='free_x')
+                        + title
+                        + axis_x
+                        + axis_y
+                        + theme(panel_spacing=0.025)
+                        )
 
-        if lines:
-            plot += geom_line(color='blue')
+            if lines:
+                plot += geom_line(color='blue')
 
-        # Save plot
-        output_dir = config.OUTPUT_DIR
-        plot_name = f"{self.current_line_name}_{y_column.lower()}.png"
-        file_path_name = os.path.join(output_dir, self.current_line_name, plot_name)
-        plot.save(filename=file_path_name, height=6, width=8, units='in', dpi=500)
+            # Save plot
+            output_dir = config.OUTPUT_DIR
+            plot_name = f"{self.current_line_name}_{y_column.lower()}.png"
+            file_path_name = os.path.join(output_dir, self.current_line_name, plot_name)
+            plot.save(filename=file_path_name
---
+++ src/modules/variables.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+#Edit these variables for command line only runs.  
+
+#what do you want your reference genome files to be labeled as? 
+reference_genome_name = 'Arabidopsis_thaliana'
+
+#What is the snpEff database you want to use? (run snpEff databases to see list. 'grep <search> | snpEff databases' to search)
+snpEff_species_db = 'Arabidopsis_thaliana'
+
+#link to reference genome download. 
+reference_genome_source = 'ftp://ftp.ensemblgenomes.org/pub/plants/release-32/fasta/arabidopsis_thaliana/dna/Arabidopsis_thaliana.TAIR10.dna.toplevel.fa.gz'
+
+# How many threads do you wish to use? set this below the max threads on your machine. 
+threads_limit = 20
+
+#cleanup intermedia files? 
+cleanup = True
+
+#vcf of known background snps. Makes your final data much more compact and clean. 
+known_snps = './references/Arabidopsis_thaliana.vcf'
+
