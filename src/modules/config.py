cf6fca2285a3dce2533013674aa9c255ddacb017
TeaShull <timshull99@icloud.com>
2024-01-11 15:59:02 -0600

    removed in progress subclass from config

--- src/modules/config.py
+++ src/modules/config.py
@@ -14,29 +14,3 @@ TEMPLATE_DIR = os.path.join(SRC_DIR,'templates')
 STATIC_DIR = os.path.join(SRC_DIR,'static')
 MODULES_DIR = os.path.join(SRC_DIR,'modules')
 
-## SUBCLASSES
-# #In progress. 
-# class ExperimentDictionary:
-#     def __init__(self):
-#         self.data = 
-#             'key': {
-#                 'required_keys': ['wt', 'mu', 'allele', 'pairedness', 'vcf_ulid', 'analysis_ulid', 
-#                                   'vcf_table_path', 'output_dir_path', 'analysis_ulid'],
-#                 'values': {}
-#             }
-
-#     def set_value(self, key, item_key, value):
-
-#         if item_key not in self.data[key]['required_keys']:
-#             raise ValueError(f"Invalid item key for {key}!")
-
-#         self.data[key]['values'][item_key] = value
-
-#     def get_value(self, key, item_key):
-#         if key not in self.data:
-#             raise ValueError("Invalid key!")
-
-#         if item_key not in self.data[key]['values']:
-#             raise ValueError(f"Item key not found for {key}!")
-
-#         return self.data[key]['values'][item_key]
