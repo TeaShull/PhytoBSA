24a78eb19539296e880cb354f9efc363e3d5ad3d
TeaShull <timshull99@icloud.com>
2024-01-26 15:17:00 -0600

    refectoring continues. Main script updated, arg parsers added. Testing soon.

--- phytobsa.py
+++ phytobsa.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python
-from settings.config import (
-    INPUT_DIR, OUTPUT_DIR, LOG_DIR
-)
 import os
 import sys
-import multiprocessing
-from datetime import datetime
 import argparse
 
-from modules.core import ThaleBSAParentFunctions
-from modules.utilities_general import FileUtilities, DictionaryUtilities
+from modules.utilities_variables import (
+    Lines, AutomaticVariables, VCFGenVariables, BSAVariables
+)
 from modules.utilities_logging import LogHandler
+from modules.core_vcf_gen import VCFGenerator
+from modules.core_bsa import BSA
 
 # Initialize core_log instance of LogHandler
 '''
@@ -32,28 +30,35 @@ Current log list:
 'analysis' - logs all messages peratining to parent_functions.bsa_analysis 
 '''
 def parse_program_arguments():
-    # Argument parsing
+    # Main parser
     parser = argparse.ArgumentParser(description='PyAtBSA main command line script...')
+    parser.add_argument('-a', '--automatic', type='store_true', help='Run PhytoBSA in automatic mode. Primary, default workflow.')
+    subparsers = parser.add_subparsers(help='Sub-command help')
 
-    ## Analysis inputs
-    parser.add_argument('-an', '--analysis', action='store_true', help='Run the analysis.')
-    parser.add_argument('-n', '--line_name', type=str, help='name of the line you wish to analyze. Will be used to name output files.')
-    parser.add_argument('-vt', '--vcf_table', type=str, help='path to the vcf table you wish to analyze.')
-    parser.add_argument('-st', '--segregation_type', type=str, help="Recessive(R) or Dominant(D)")
+    ## Analysis subparser
+    parser_analysis = subparsers.add_parser('analysis', help='Run PhytoBSA analysis seperately.')
+    ### REQUIRED
+    parser_analysis.add_argument('-n', '--name', required=True, type=str, help='name of the line you wish to analyze. Will be used to name output files.')
+    parser_analysis.add_argument('-vt', '--vcf_table', required=True, type=str, help='path to the vcf table you wish to analyze.')
+    parser_analysis.add_argument('-st', '--segregation_type', required=True, type=str, help="Recessive(R) or Dominant(D)")
+    ### OPTIONAL
+    parser_analysis.add_argument('-ls', '--loess_span', type=float, default=0.3, help="Influences smoothing parameters.")
+    parser_analysis.add_argument('-si', '--shuffle_iterations', type=int, default=1000, help="Iterations of bootstrapping during empirical cutoff calculations. Below 1000 can yeild inconsistant results")
+    parser_analysis.add_argumment('-sb', '--smooth_edges_bounds', type=int, default=15, help="Number of mirrored datapoints at chromosome edges to correct for loess edge bias. Increase if edge bias seems high")
 
-    ## VCF generation inputs
-    reference_genome_name_help = f"""
-        What is the name of your reference genome? this should be the base name of your fasta file. 
-        example - Arabidopsis_thaliana.fa 
-        reference_genome_name = Arabidopsis_thaliana""" 
-    parser.add_argument('-rgn', '--reference_genome_name', default=None,
-                        type=str, help=reference_genome_name_help)
-    snpEff_species_db_help = ("What is the name of your snpEff database for your reference genome?")
-    parser.add_argument('-ssdb', '--snpEff_species_db', default=None, type=str)
-    parser.add_argument('-rgs', '--reference_genome_source', default=None, type=str)
-    parser.add_argument('-t', '--threads_limit', default=None, type=str)
-    parser.add_argument('-c','--cleanup', default=None, type=str)
-    parser.add_argument('-ks','--known_snps', default=None, type=str)
+    ## VCF generation subparser
+    parser_vcf_gen = subparsers.add_parser('vcf_generator', help='Run PhytoBSA VCF Generator seperately')
+    ### REQUIRED
+    parser_vcf_gen.add_argument('-rgn', '--reference_genome_name', required=True, default=None, type=str, help='Reference genome name')
+    parser_vcf_gen.add_argument('-wt', '--wt_input', required=True, default = None, type=str, help='Wild-type bulk fasta file(s)')
+    parser_vcf_gen.add_argument('-mu', '--mu_input', required=True, default=None, type=str, help='mutant bulk fast files')
+    ### OPTIONAL - the below arguments can be input via command line or can be sourced from ./settings/vcf_gen_variables.py
+    parser_vcf_gen.add_argument('-rgs', '--reference_genome_source', default=None, type=str, help = 'Optional, if you wish the pipeline to download your reference from a url')
+    parser_vcf_gen.add_argument('-ssdb', '--snpEff_species_db', default=None, type=str, help = 'The name of your snpEff database name.')
+    parser_vcf_gen.add_argument('-ks','--known_snps', default=None, type=str, help = 'VCF file containing background SNPs. Helps improve output quality')
+    parser_vcf_gen.add_argument('-t', '--threads_limit', default=None, type=str, help='Maximum threads you wish to use for analysis')
+    parser_vcf_gen.add_argument('-p', '--call_variants_in_parallel', default=False, type=bool, help='Run gatk haplotype caller in parallel')
+    parser_vcf_gen.add_argument('-c','--cleanup', default=None, type=bool, help='If true, intermediate files will de deleted. False for troubleshooting and archiving files.' )
     args = parser.parse_args()
     return args
 
@@ -63,63 +68,51 @@ def main():
     core_log.note(f'Core log begin. ulid: {core_log.ulid}')
     core_log.add_db_record()
     
-    # Create instances of ThaleBSAParentFunctions and FileUtilities. 
-    file_utils = FileUtilities(core_log)
-
+    # Parse command line arguments
     args = parse_program_arguments()
-    # parse command line arguments
-    line_name = args.line_name
-    vcf_table = args.vcf_table
-    reference_genome_name = args.reference_genome_name
-    snpEff_species_db = args.snpEff_species_db
-    reference_genome_source = args.reference_genome_source
-    threads_limit = args.threads_limit
-    cleanup = args.cleanup
-    known_snps = args.known_snps
-    segregation_type = args.segregation_type
+
+    # Determine which routine to run
+    if args.command == 'analysis':
+        line = Lines(core_log, args.name)
+        
+        line.usr_in_variables(args.vcf_table, args.segregation_type)
+        bsa_vars = BSAVariables(core_log, 
+            lines=line, 
+            args.loess_span, 
+            args.smooth_edges_bounds, 
+            args.shuffle_iterations
+        )
     
+        bsa = BSA(core_log, bsa_vars)    
+        bsa.run_pipeline()
+    
+    elif args.command == 'vcf_generator':
+        line = Lines(core_log, args.name)
+        
+        line.usr_in_variables(args.reference_genome_name, args.wt_input, args.mu_input)
+        vcf_gen_vars = VCFGenVariables(core_log, 
+            lines=line,
+            args.reference_genome_source, 
+            args.snpEff_species_db, 
+            args.known_snps, 
+            args.threads_limit, 
+            args.call_variants_in_parallel,
+            args.cleanup
+        )
 
-    # Check if user wants to the command line and variables.py instead of the Flask app.
-    core_log.attempt('Parsing command line arguments...')
-    try:
-        # [if -an arg] accept line name and vcf table to run bsa_analysis 
-        if args.analysis:
-            core_log.note('Command line argument to run analysis detected.')
-            core_log.attempt(f'Trying to create experiment_dictionary from arguments...')
-            try:
-                if line_name and vcf_table and segregation_type:
-                    experiment_dictionary=parent_functions.populate_experiment_dictionary(
-                        line_name, vcf_table, allele
-                    )
-                    core_log.success(f'experiment_dictionary successfully created')
-                else: 
-                    core_log.fail('-ln, -vt or -al not set. Aborting...')
-            
-            except Exception as e:
-                core_log.fail('There was a failure trying to create experiment_dictionary from passed arguments:{e}')
-            
-            core_log.attempt(f'Attempting to begin analysis of {args.vcf_table}...')
-            try: 
-                parent_functions.bsa_analysis(experiment_dictionary)
-                quit()
-            except Exception as e:
-                core_log.fail(f'There was an error while trying to start bsa_analysis:{e}')
+        vcf_gen = VCFGenerator(core_log, vcf_gen_vars)
+        vcf_gen.run_subprocess()
 
-        if not args.analysis:
-            core_log.attempt('No command line arguments given. Running automatic command line operations.')
-            dict_utils = DictionaryUtilities(core_log) #Initiate dict_utils
-            dict_utils.experiment_detector() # Detect experiments in ./inputs
-            dict_utils.check_vcf_gen_variables() #Source variables from settings/vcf_gen_variables.py if not passed by user
-            dict_utils.generate_output_file_paths() #Generate neccissary output paths before running pipeline
-            
-            parent_functions = ThaleBSAParentFunctions(core_log, dict_utils)
-            parent_functions.vcf_generation() #Generate VCF file to analyze
-            parent_functions.bsa_analysis() #Analyze VCF file
-            quit()
+    elif not args.command
+        auto_vars = AutomaticVariables(core_log)
 
-    except Exception as e:
-        core_log.fail(f'Starting thaleBSA has failed: {e}')
-        quit()
+        vcf_vars = VCFGenVariables(core_log, auto_vars.lines)
+        vcf_gen = VCFGenerator(core_log, vcf_vars)
+        vcf_gen.run_subprocess()
+        
+        bsa_vars = BSAVariables(core_log, lines=vcf_gen.lines)
+        bsa = BSA(core_log, bsa_vars)
+        bsa.run_pipeline()
 
-if __name__ == '__main__':
-    sys.exit(main())
+if __name__ == "__main__":
+    main()
